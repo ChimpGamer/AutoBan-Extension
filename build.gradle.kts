aae99a397d3dd89193187366ea7a8c2f01fd3efa
ChimpGamer <Tim.Cremers@Ziggo.nl>
2021-12-17 20:30:27 +0100

    Update kotlin to 1.6.10, update shadow to 7.1.1, removed bungeecord api and bumped version to 1.0.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.5.21"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
 }
 
 repositories {
@@ -14,13 +14,12 @@ repositories {
 
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
-    compileOnly("net.md-5:bungeecord-api:1.16-R0.4")
     compileOnly("com.github.Carleslc:Simple-YAML:1.7.2")
-    compileOnly("nl.chimpgamer.networkmanager:api:2.10.0")
+    compileOnly("nl.chimpgamer.networkmanager:api:2.10.6")
 }
 
 group = "nl.chimpgamer.networkmanager.extensions"
-version = "1.0.11"
+version = "1.0.12"
 description = "AutoBan"
 
 tasks {
